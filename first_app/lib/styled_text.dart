3d9353bf7513088afaf680cf7514c7d732a50892
amcny <cny2045@gmail.com>
2024-07-30 19:50:32 +0530

    ch

--- first_app/lib/styled_text.dart
+++ first_app/lib/styled_text.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText(String text, {super.key});
-
+  const StyledText(String text, {super.key}) : outputtext = text;
+  String outputtext;
   @override
   Widget build(context) {
     return const Text(
-      'Hello World!',
+      outputtext,
       style: TextStyle(
         color: Colors.white,
         fontSize: 28,
