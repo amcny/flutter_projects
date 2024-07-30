d7f362c18ee577f4cf4210cdf751c4559c4658ba
amcny <cny2045@gmail.com>
2024-07-30 19:55:35 +0530

    change const

--- first_app/lib/styled_text.dart
+++ first_app/lib/styled_text.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  StyledText(this.text, {super.key});
+  const StyledText(this.text, {super.key});
 
   final String text;
 
